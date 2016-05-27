6732fc78c9355b16b986ffacb630f59dd5d0ff4c
loTEDve <loTEDve@GMail.CoM>
2016-05-27 19:27:27 +0300

    Исправлена ошибка, изза которой после окончания матча панель победителя могла становиться прозрачной из-за актуальной цели

--- Interface/TargetFrame.lua
+++ Interface/TargetFrame.lua
@@ -75,7 +75,7 @@ end
 
 
 function ezSpectator_TargetFrame:Update(LockAnimation)
-	if self.Parent.Interface.Viewpoint then
+	if self.Parent.Interface.Viewpoint and self.Parent.Interface.IsRunning then
 		self.Parent.Interface.Viewpoint.SmallFrame.Target.Unit.SmallFrame:SetAlpha(self.Parent.Data.ViewpointAlpha)
 	end
 
